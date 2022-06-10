31312833f9afe30157f6abe3335f588fb355890c
Andrew Pavlovskiy <starlex@Andrews-MacBook-Pro.local>
2022-06-10 18:27:03 +0300

    javascript modules added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
