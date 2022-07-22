ec6cf954e0cc053a6e72b2e276801e8800a43369
Starlexxx <apavlovskiy088@gmail.com>
2022-07-22 09:28:50 +0300

    hw-16 done

--- db/migrate/20220708151432_create_feedbacks.rb
+++ db/migrate/20220708151432_create_feedbacks.rb
@@ -1,6 +1,9 @@
 class CreateFeedbacks < ActiveRecord::Migration[6.1]
   def change
     create_table :feedbacks do |t|
+      t.string :name
+      t.string :email
+      t.string :message
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_08_154110) do
+ActiveRecord::Schema.define(version: 2022_07_08_151432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,11 +31,11 @@ ActiveRecord::Schema.define(version: 2022_07_08_154110) do
   end
 
   create_table "feedbacks", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "email"
     t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "gists", force: :cascade do |t|
